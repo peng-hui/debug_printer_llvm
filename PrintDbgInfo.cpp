83bf85b8b9c415e818536445c9baf5faeb42f07c
Penghui Li <phli@cse.cuhk.edu.hk>
2021-11-12 17:52:43 +0800

    clean code, use compatiable api, remove loop

--- PrintDbgInfo.cpp
+++ PrintDbgInfo.cpp
@@ -13,17 +13,15 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Pass.h"
-#include "llvm/Function.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/InstIterator.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/ADT/Statistic.h"
-#include "llvm/DebugInfo.h"
-#include "llvm/Support/type_traits.h"
-#include "llvm/ADT/StringRef.h"
+#include "llvm/IR/DebugInfo.h"
+// #include "llvm/IR/DebugInfoMetadata.h"
+// #include "llvm/Support/type_traits.h"
 
 using namespace llvm;
-
-StringRef * loopCond= new StringRef ("for.cond");
-int loop_count = 0;
 
 namespace {
   
@@ -34,28 +32,22 @@ namespace {
     virtual bool runOnFunction(Function &F) {
 	if (F.isDeclaration())
 		     return false;
-	for (Function::iterator i = F.begin(), e = F.end(); i != e; ++i)
-	{
-		
-		if(!((i->getName().find(*loopCond,0))))  //the first basic block of for loop
-		{
-			loop_count++;
-			for (BasicBlock::const_iterator I = i->begin(), E = i->end();
-		        	  I != E; ++I) {
+        for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
+            const DILocation *DIL = I->getDebugLoc();
+            if (!DIL)
+                continue;
+            errs() << DIL->getFilename()<< " ==  " << DIL->getLine() <<"\n";
+        }
 
-				//get the line info for the first instruction of the basic block
-				if (MDNode *N = I->getMetadata("dbg")) {  // Here I is an LLVM instruction
-					DILocation Loc(N);                      // DILocation is in DebugInfo.h
-					unsigned Line = Loc.getLineNumber();
-					StringRef File = Loc.getFilename();
-					//StringRef Dir = Loc.getDirectory();
-					errs()<<loop_count<<" "<<Line<<" "<<File<<"\n";
-					break;
-				}//end of if
-			}//end of for
-		}//end of if the first basic block of loop
-	}
-      return false;
+        for (BasicBlock &BB : F) {
+            for (Instruction &I : BB) {
+                const DILocation *DIL = I.getDebugLoc();
+                if (!DIL)
+                    continue;
+                errs() << DIL->getFilename()<< " --  " << DIL->getLine() <<"\n";
+            }
+        }
+        return false;
     }
 
     // We don't modify the program, so we preserve all analyses
